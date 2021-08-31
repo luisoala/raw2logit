09b5b92ade01353e9d5e1de34b86c5a05f315f4d
willis <willis@campus.tu-berlin.de>
2021-08-31 17:55:00 +0200

    reorganize

--- figure1.sh
+++
@@ -1,7 +0,0 @@
-python figures.py \
---experiment_name track-test \
---run_name track-all \
---representation gradients \
---step gamma_correct \
---gif_name gradient \
---output gif \
--- figure2.sh
+++
@@ -1,4 +0,0 @@
-python figures.py \
---experiment_name track-test \
---run_name track-all \
---output train_vs_val_loss \
